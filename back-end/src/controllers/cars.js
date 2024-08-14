aaafeb6bb093ee39e2747e430ef18e627099bef0
Marcelo José Martins Pereira <marjopereira10@gmail.com>
2024-08-14 09:08:56 -0300

    :sparkles: feat: Adição da pasta controllers

--- back-end/src/controllers/cars.js
+++ back-end/src/controllers/cars.js
@@ -2,4 +2,19 @@ import prisma from '../database/client.js';
 
 const controller = {}   // Objeto vazio
 
+controller.create = async function(req, res) {
+    try {
+        await prisma.car.create({data: req.body});
+        
+        // HTTP 201 Created
+        res.status(201).end();
+    }
+    catch (error) {
+        console.error(error);
+        
+        // HTTP 500 Internal Server Error
+        res.status(500).end();
+    }
+}
+
 export default controller; // Exporta o objeto vazio
