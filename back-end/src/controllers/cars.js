b59ae8290f0798d6503df027ea846cfa898be280
CaioMonteiroSouza <caioeduardo1403@gmail.com>
2024-08-21 09:09:12 -0300

    21/08 Adição do Update e Delete (Delete está com Bug)

--- back-end/src/controllers/cars.js
+++ back-end/src/controllers/cars.js
@@ -1,4 +1,5 @@
 import prisma from '../database/client.js'
+import { Prisma } from '@prisma/client'
 
 const controller = {}
 
@@ -28,5 +29,59 @@ controller.retrieveAll = async function(req, res) {
     }
 }
 
+controller.retrieveOne = async function (req, res) {
+    try {
+        const result = await prisma.car.findUnique({
+            where: {id: Number(req.params.id)}
+        })
+
+        if (result) res.send(result)
+        else res.status(404).end()
+    }
+    catch(error) {
+        console.error(error)
+
+        res.status(500).end()
+    }
+}
+
+controller.update = async function (req, res) {
+    try {
+        const result = await prisma.car.update({
+            where: { id: Number(req.params.id) },
+            data: req.body
+        })
+
+        if(result) res.status(204).end()
+
+        else res.status(404).end()
+    }
+    catch(error) {
+        console.error(error)
+
+        res.status(500).end()
+    }
+    
+}
+
+controller.delete = async function (req, res) {
+    try {
+        await prisma.car.delete({
+            where: {id: Number(req.params.id)}
+        })
+
+        res.status(204).end()
+    }
+    catch(error){
+        if(error instanceof Prisma.RecordNotFound) {
+            res.status(404).end()
+        }
+        else{
+            console.error(error)
+            
+            res.status(500).end()
+        }
+    }
+}
 
 export default controller
