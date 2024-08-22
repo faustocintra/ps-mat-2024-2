adcbd05f9d59c8193916bf6782d13fdbcf455161
Murilo Sanches <zmurilo2010@gmail.com>
2024-08-22 11:26:45 -0300

    (22/08) Término do primeiro CRUD

--- back-end/src/controllers/cars.js
+++ back-end/src/controllers/cars.js
@@ -1,4 +1,5 @@
 import prisma from "../database/client.js";
+import { Prisma } from "@prisma/client";
 
 const controller = {}; // Objeto vazio
 
@@ -20,13 +21,69 @@ controller.retrieveAll = async function (req, res) {
   try {
     const result = await prisma.car.findMany();
 
-    // HTTP 200: OK
     res.send(result);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).end();
+  }
+};
+
+controller.retrieveOne = async function (req, res) {
+  try {
+    const result = await prisma.car.findUnique({
+      where: { id: Number(req.params.id) },
+    });
+
+    // HTTP 200: OK
+    if (result) res.send(result);
+    // Não encontrou ~> retorna HTTP 404: Not Found
+    else res.status(404).end();
+  } catch (error) {
+    // HTTP 500: Internal Server Error
+    console.error(error);
+
+    // HTTP 500: Internal Server Error
+    res.status(500).end();
+  }
+};
+
+controller.update = async function (req, res) {
+  try {
+    const result = await prisma.car.update({
+      where: { id: Number(req.params.id) },
+      data: req.body,
+    });
+    // Encontrou e atualizou ~> HTTP 204: No Content
+    if (result) res.status(204).end();
+    // Nao encontrou (e não atualizou) ~ HTTP 404: Not Found
+    else res.status(404).end();
   } catch (error) {
     console.error(error);
 
     // HTTP 500: Internal Server Error
     res.status(500).end();
+  }
+};
+
+controller.delete = async function (req, res) {
+  try {
+    await prisma.car.delete({
+      where: { id: Number(req.params.id) },
+    });
+
+    // Encontrou e excluiu ~> HTTP 204: Not Found
+    res.status(204).end();
+  } catch (error) {
+    if (error?.code === "P2025") {
+      res.status(404).end();
+    } else {
+      // Outros tipos de erro
+      console.error(error);
+
+      // HTTP 500: Internal Server Error
+      res.status(500).end();
+    }
   }
 };
 
--- back-end/src/database/client.js
+++ back-end/src/database/client.js
@@ -1,5 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: [{ emit: "event", level: "query" }],
+});
+
+prisma.$on("query", (event) => {
+  console.log("%".repeat(60));
+  console.log(event.query);
+  if (event.params) console.log("PARAMS:", event.params);
+  console.log("%".repeat(60));
+});
 
 export default prisma;
