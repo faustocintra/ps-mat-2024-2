8172b3f5b729d5b8ef61d680dfbddf0f78a129e2
Fausto G. Cintra <professor@faustocintra.com.br>
2024-08-22 11:31:37 -0300

    (22/08) Término do primeiro CRUD

--- back-end/src/controllers/cars.js
+++ back-end/src/controllers/cars.js
@@ -32,4 +32,67 @@ controller.retrieveAll = async function(req, res) {
   }
 }
 
+controller.retrieveOne = async function(req, res) {
+  try {
+    const result = await prisma.car.findUnique({
+      where: { id: Number(req.params.id) }
+    })
+
+    // Encontrou ~> retorna HTTP 200: OK (implícito)
+    if(result) res.send(result)
+    // Não encontrou ~> retorna HTTP 404: Not Found
+    else res.status(404).end()
+  }
+  catch(error) {
+    console.error(error)
+
+    // HTTP 500: Internal Server Error
+    res.status(500).end()
+  }
+}
+
+controller.update = async function(req, res) {
+  try {
+    const result = await prisma.car.update({
+      where: { id: Number(req.params.id) },
+      data: req.body
+    })
+
+    // Encontrou e atualizou ~> HTTP 204: No Content
+    if(result) res.status(204).end()
+    // Não encontrou (e não atualizou) ~> HTTP 404: Not Found
+    else res.status(404).end()
+  }
+  catch(error) {
+    console.error(error)
+
+    // HTTP 500: Internal Server Error
+    res.status(500).end()
+  }
+}
+
+controller.delete = async function(req, res) {
+  try {
+    await prisma.car.delete({
+      where: { id: Number(req.params.id) }
+    })
+
+    // Encontrou e excluiu ~> HTTP 204: No Content
+    res.status(204).end()
+  }
+  catch(error) {
+    if(error?.code === 'P2025') {
+      // Não encontrou e não excluiu ~> HTTP 404: Not Found
+      res.status(404).end()
+    }
+    else {
+      // Outros tipos de erro
+      console.error(error)
+
+      // HTTP 500: Internal Server Error
+      res.status(500).end()
+    }
+  }
+}
+
 export default controller
