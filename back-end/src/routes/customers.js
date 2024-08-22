17ee83c1340e04cd75bb989a318ea877455535c1
Marcelo José Martins Pereira <marjopereira10@gmail.com>
2024-08-22 12:34:01 -0300

    (22/08) Começo do projeto adcionando customer

---
+++ back-end/src/routes/customers.js
@@ -0,0 +1,12 @@
+import e, { Router } from 'express';
+import controller from '../controllers/customers.js';
+
+const router = Router();
+
+router.post('/', controller.create);
+router.get('/', controller.retriveAll);
+router.get('/:id', controller.retrieveOne)
+router.put('/:id', controller.update)
+router.delete('/:id', controller.delete)
+
+export default router;
