ceee92d79020d5eef0aae03a4fe297d4bc9b380b
Fausto G. Cintra <professor@faustocintra.com.br>
2024-08-29 12:43:33 -0300

    (29/08) Início da implementação da lógica de usuários

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,9 +1,12 @@
+import dotenv from 'dotenv'
+dotenv.config() // Carrega as variáveis de ambiente do arquivo .env
+
 import express, { json, urlencoded } from 'express'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 
 import indexRouter from './routes/index.js'
-import usersRouter from './routes/users.js'
+//import usersRouter from './routes/users.js'
 
 const app = express()
 
@@ -13,11 +16,14 @@ app.use(urlencoded({ extended: false }))
 app.use(cookieParser())
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
+//app.use('/users', usersRouter)
 
 /*********** ROTAS DA API **************/
 
 import carsRouter from './routes/cars.js'
 app.use('/cars', carsRouter)
+
+import usersRouter from './routes/users.js'
+app.use('/users', usersRouter)
 
 export default app
