8a77c01ad53c2e227b3650e4417ae1dd21e3726e
Murilo Sanches <zmurilo2010@gmail.com>
2024-08-29 12:43:16 -0300

    (29/08) Início da implementação da lógica de usuarios

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,9 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
+
 import express, { json, urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 
 import indexRouter from "./routes/index.js";
-import usersRouter from "./routes/users.js";
+// import usersRouter from "./routes/users.js";
 
 const app = express();
 
@@ -13,7 +16,7 @@ app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+// app.use("/users", usersRouter);
 
 /********* ROTAS DA API ***********/
 
@@ -22,5 +25,8 @@ app.use("/cars", carsRouter);
 
 import customersRouter from "./routes/customers.js";
 app.use("/customers", customersRouter);
+
+import usersRouter from "./routes/users.js";
+app.use("/users", usersRouter);
 
 export default app;
