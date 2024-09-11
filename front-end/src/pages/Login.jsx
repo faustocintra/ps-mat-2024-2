87c29170c76e58dc014d3cb3258b8a28204703b5
arturxdking <arturxdking@gmail.com>
2024-09-11 09:09:20 -0300

    (11/09) Configuração do CORS no back-end

---
+++ front-end/src/pages/Login.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import {
+    Typography,
+    Paper,
+    TextField,
+    InputAdornment,
+    Button,
+    IconButton
+} from '@mui/material'
+
+export default function Login() {
+
+    const [state, setState] = React.useState({
+        email: '',
+        password: ''
+    })
+    const {
+        email,
+        password
+    } = state
+
+    return (
+        <>
+            <Typography variant="h1" gutterBottom>
+                Autentique-se
+            </Typography>
+
+            <Paper
+                elevation={6}
+                sx={{
+                    padding: '24px',
+                    maxWidth: '500px',
+                    margin: 'auto'
+                }}
+            >
+                <form>
+                    <TextField
+                        name="email"
+                        value={email}
+                        label="E-mail"
+                        variant="filled"
+                        fullWidth
+                        sx={{ mb: '24px' /* mb = marginBottom */ }}
+                    />
+
+                    <TextField
+                        name="email"
+                        value={password}
+                        label="Senha"
+                        variant="filled"
+                        type="password"
+                        fullWidth
+                        sx={{ mb: '24px' /* mb = marginBottom */ }}
+                    />
+
+                    <Button
+                        variant="contained"
+                        type="submit"
+                        color="secondary"
+                        fullWidth
+                    >
+                        Enviar
+                    </Button>
+                </form>
+            </Paper>
+        </>
+    )
+}
