ca4141e99f409cc2e4e3474fea009e2e47e5bbd7
arturxdking <arturxdking@gmail.com>
2024-09-12 12:46:20 -0300

    (12/09) Finalização da página de login

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -8,19 +8,84 @@ import {
     IconButton
 } from '@mui/material'
 
+import {
+    Visibility,
+    VisibilityOff
+} from '@mui/icons-material'
+import myfetch from '../lib/myfetch'
+import useNotifiction from '../ui/useNotification'
+import useWaiting from '../ui/useWaiting'
+import { useNavigate } from 'react-router-dom'
+import useNotification from '../ui/useNotification'
+
 export default function Login() {
 
     const [state, setState] = React.useState({
         email: '',
-        password: ''
+        password: '',
+        showPassword: false
     })
     const {
         email,
-        password
+        password,
+        showPassword
     } = state
+
+    const { notify, Notification } = useNotification()
+    const { showWaiting, Waiting } = useWaiting()
+
+    const navigate = useNavigate()
+
+    function handleChange(event) {
+        // Atualiza a variável de estado associada ao 
+        // input que foi modificado
+        setState({ ...state, [event.target.name]: event.target.value })
+    }
+
+    function handleClick(event) {
+        /* Alterna avisibilidade da senha */
+        setState({ ...state, showPassword: !showPassword })
+    }
+
+    async function handleSubmit(event) {
+        event.preventDefault()      // Evita o recarregamento da página
+        showWaiting(true)
+        try {
+
+            const loginData = {password}
+
+            if(email.includes('@')) loginData.email = email
+            // Se o valor da variável email não contiver @, será tratado como um suername
+            else loginData.username = email
+
+            console.log({loginData})
+
+            // Envia email e password para o back-end para fazer autenticação
+            const response = await myfetch.post('/users/login', loginData)
+
+            // Armazena o token retornado no localStorage(INSEGURO!)
+            window.localStorage.setItem(
+                import.meta.env.VITE_AUTH_TOKEN_NAME,
+                response.token
+            )
+
+            // Mostra a notificação de sucesso e depois vai para a página inicial
+            notify('Autenticação realizada com sucesso', 'success', 1500, () => navigate('/'))
+
+        } catch (error) {
+            console.error(error)
+            notify(error.message, 'error')
+        } finally {
+            showWaiting(false)
+        }
+    }
 
     return (
         <>
+
+            <Notification />
+            <Waiting />
+
             <Typography variant="h1" gutterBottom>
                 Autentique-se
             </Typography>
@@ -33,24 +98,38 @@ export default function Login() {
                     margin: 'auto'
                 }}
             >
-                <form>
+                <form onSubmit={handleSubmit}>
                     <TextField
                         name="email"
                         value={email}
-                        label="E-mail"
+                        label="Nome de usuário ou e-mail"
                         variant="filled"
                         fullWidth
+                        onChange={handleChange}
                         sx={{ mb: '24px' /* mb = marginBottom */ }}
                     />
 
                     <TextField
-                        name="email"
+                        name="password"
                         value={password}
                         label="Senha"
                         variant="filled"
-                        type="password"
+                        type={showPassword ? 'text' : 'password'}
                         fullWidth
+                        onChange={handleChange}
                         sx={{ mb: '24px' /* mb = marginBottom */ }}
+                        InputProps={{
+                            endAdornment:
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="alterna a visibilidade da senha"
+                                        onClick={handleClick}
+                                        edge="end"
+                                    >
+                                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                        }}
                     />
 
                     <Button
