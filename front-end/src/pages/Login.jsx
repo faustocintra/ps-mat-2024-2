9adf8a8f97279a2af9d820ac6083282e4e01ec46
Marcelo José Martins Pereira <marjopereira10@gmail.com>
2024-09-12 12:46:00 -0300

    (12/09) Finalização da página de login

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -7,20 +7,79 @@ import {
   Button,
   IconButton
 } from '@mui/material'
+import {
+  Visibility,
+  VisibilityOff
+} from '@mui/icons-material'
+import myFetch from '../lib/myfetch'
+import useNotification from '../ui/useNotification'
+import useWaiting from '../ui/useWaiting'
+import { useNavigate } from 'react-router-dom'
 
 export default function Login() {
 
   const [state, setState] = React.useState({
     email: '',
-    password: ''
+    password: '',
+    showPassword: false
   })
   const {
     email,
-    password
+    password,
+    showPassword
   } = state
+
+  const { notify, Notification} = useNotification()
+  const { showWaiting, Waiting } = useWaiting()
+
+  const navigate = useNavigate()
+
+  function handleChange(event) {
+    // Atualiza a variável de estado associada ao
+    // input que foi modificado
+    setState({ ...state, [event.target.name]: event.target.value })
+  }
+
+  function handleClick(event) {
+    // Alterna a visibilidade da senha
+    setState({ ...state, showPassword: !showPassword })
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault() // Evita o recarregamento da página
+    showWaiting(true)
+    try {
+      const loginData = {password}
+
+      // Se o valor do email não tiver @ tratar como usurmame
+      if (email.includes('@')) loginData.email = email
+      else loginData.username = email
+
+      // Envia email e password para o back-end para fazer autenticação
+      const response = await myFetch.post('/users/login', 'POST', loginData)
+
+      // Armazena o token retornado no local storage (inseguro)
+      window.localStorage.setItem(import.meta.env.VITE_AUTH_TOKEN_NAME, response.token)
+
+      // Mostra a notificação de sucesso e depois redireciona para a página inicial
+      notify('Autenticação realizada com sucesso', 'success', 1500,
+        () => navigate('/'))
+    }
+
+    catch(error){
+      console.error(error)
+      notify(error.message, 'error')
+    }
+    finally {
+      showWaiting(false)
+    }
+  }
 
   return(
     <>
+      <Notification />
+      <Waiting />
+      
       <Typography variant="h1" gutterBottom>
         Autentique-se
       </Typography>
@@ -33,13 +92,14 @@ export default function Login() {
           margin: 'auto'
         }}
       >
-        <form>
+        <form onSubmit={handleSubmit}>
           <TextField
             name="email"
             value={email}
-            label="E-mail"
+            label="Nome de usuário ou email"
             variant="filled"
             fullWidth
+            onChange={handleChange}
             sx={{ mb: '24px' /* mb = marginBottom */ }}
           />
 
@@ -48,9 +108,22 @@ export default function Login() {
             value={password}
             label="Senha"
             variant="filled"
-            type="password"
+            type={ showPassword ? 'text': 'password' }
             fullWidth
+            onChange={handleChange}
             sx={{ mb: '24px' /* mb = marginBottom */ }}
+            InputProps={{
+              endAdornment:
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="alterna a visibilidade da senha"
+                    onClick={handleClick}
+                    edge="end"
+                  >
+                    { showPassword ? <VisibilityOff /> : <Visibility /> }
+                  </IconButton>
+                </InputAdornment>
+            }}
           />
 
           <Button
