39ecca8b4ebc799daf4d79f70fd402e373e43343
Fausto G. Cintra <professor@faustocintra.com.br>
2024-09-19 12:34:58 -0300

    (19/09) Término da proteção de rotas e menus

--- front-end/src/routes/AppRoutes.jsx
+++ front-end/src/routes/AppRoutes.jsx
@@ -20,16 +20,16 @@ export default function AppRoutes() {
 
     <Route path="/login" element={ <Login /> } />
 
-    <Route path="/cars" element={ <CarList /> } />
-    <Route path="/cars/new" element={ <CarForm /> } />
-    <Route path="/cars/:id" element={ <CarForm /> } />
+    <Route path="/cars" element={ <AuthGuard> <CarList /> </AuthGuard> } />
+    <Route path="/cars/new" element={ <AuthGuard> <CarForm /> </AuthGuard> } />
+    <Route path="/cars/:id" element={ <AuthGuard> <CarForm /> </AuthGuard> } />
 
     <Route path="/customers" element={ 
       <AuthGuard> <CustomerList /> </AuthGuard> 
     } />
     
-    <Route path="/customers/new" element={ <CustomerForm /> } />
-    <Route path="/customers/:id" element={ <CustomerForm /> } />
+    <Route path="/customers/new" element={ <AuthGuard> <CustomerForm /> </AuthGuard>} />
+    <Route path="/customers/:id" element={ <AuthGuard> <CustomerForm />  </AuthGuard> } />
 
     {/* 11. Crie uma rota para esse componente. */}
     <Route path="/about" element={ <About /> } />
